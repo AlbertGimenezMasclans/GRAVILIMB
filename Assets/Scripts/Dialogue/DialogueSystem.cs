855d92d05dc70a73e644346374b7ac0eb5d9aa91
AlbertGimenezMasclans <150661196+AlbertGimenezMasclans@users.noreply.github.com>
2025-03-16 21:02:29 +0100

    Sistema de Diàlogos Implementado
    Ya esta implementado el sistema de dialogos via inspector, es muy comodo de usar e intuitivo.
    
    Aún esta por finalizar, pero por ahora, funciona perfectamente, solo hay que pulir y hace algunos añadidos.

---
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using TMPro;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{   
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject textBox;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(1, 4)] private string[] dialogueLines;
+
+    private float typingTime = 0.065f;
+
+    private bool isPlayerRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerRange && Input.GetKeyDown(KeyCode.C))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+            else if(dialogueText.text == dialogueLines[lineIndex])
+            {
+                NextDialogueLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        textBox.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            textBox.SetActive(false);
+            dialogueMark.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerRange = true;
+            dialogueMark.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerRange = false;
+            dialogueMark.SetActive(false);
+        }
+    }
+}
