26f07a3b445817147023cad4b7da9dd71341b9dd
AlbertGimenezMasclans <150661196+AlbertGimenezMasclans@users.noreply.github.com>
2025-03-21 10:39:18 +0100

    Animación + Efecto Kred Tokens
    Ahora, las monedas tienen una animación y he creado un efecto que aparece justo en la posición de la moneda, pasando por encima del personaje y quedando bastante bonito.

--- Assets/Scripts/Coins/KredTokens.cs
+++ Assets/Scripts/Coins/KredTokens.cs
@@ -3,14 +3,33 @@ using UnityEngine;
 public class KredToken : MonoBehaviour
 {
     private int tokenValue = 1000; // Valor de cada Kred Token
+    [SerializeField] private GameObject collectionEffectPrefab; // Prefab del efecto de recolección
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Notificar al CoinManager que se recogió una moneda
+            if (collectionEffectPrefab != null)
+            {
+                GameObject effect = Instantiate(collectionEffectPrefab, transform.position, Quaternion.identity);
+                // Activar Flip X aleatoriamente (50% de probabilidad)
+                SpriteRenderer effectRenderer = effect.GetComponent<SpriteRenderer>();
+                if (effectRenderer != null)
+                {
+                    effectRenderer.flipX = Random.value > 0.5f; // True o False aleatorio
+                }
+                else
+                {
+                    Debug.LogWarning("El prefab KredCollect no tiene SpriteRenderer.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("CollectionEffectPrefab no está asignado en el Inspector del KredToken.");
+            }
+
             KredsManager.Instance.AddTokens(tokenValue);
-            Destroy(gameObject); // Destruir la moneda
+            Destroy(gameObject);
         }
     }
 }
